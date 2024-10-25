f41ad2065a56b4bd118003c650f60c60067cf08c
julmax <you@example.com>
2024-10-25 04:11:13 +0300

    removed .gitignore dirs/files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Max/Desktop/osi/jlabs/labs-osi-floryajulya
-# Build directory: /mnt/c/Users/Max/Desktop/osi/jlabs/labs-osi-floryajulya/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lab2")
-subdirs("tests")
--- build/lab2/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Max/Desktop/osi/jlabs/labs-osi-floryajulya/lab2
-# Build directory: /mnt/c/Users/Max/Desktop/osi/jlabs/labs-osi-floryajulya/build/lab2
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
